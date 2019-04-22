ad8cc86f953e99d1c60a9073b82b02c5c7df1059
Franky's iMac <548913086@qq.com>
2019-04-22 19:51:27 +0800

    提交

---
+++ SimpleApp/WKWebViewController.h
@@ -0,0 +1,18 @@
+//
+//  WKWebViewController.h
+//  SimpleApp
+//
+//  Created by Franky on 2019/4/22.
+//  Copyright © 2019 Franky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKWebViewController : UIViewController
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SimpleApp/WKWebViewController.m
@@ -0,0 +1,34 @@
+//
+//  WKWebViewController.m
+//  SimpleApp
+//
+//  Created by Franky on 2019/4/22.
+//  Copyright © 2019 Franky. All rights reserved.
+//
+
+#import "WKWebViewController.h"
+
+@interface WKWebViewController ()
+
+@end
+
+@implementation WKWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
